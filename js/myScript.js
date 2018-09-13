325c691a6d514a6e74c5e257657843b596bbebe4
Marcos Souza <37846149+marcossouz@users.noreply.github.com>
2018-09-12 21:33:01 -0300

    update 2

---
+++ js/myScript.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+	document.getElementById("demo").innerHTML = "A Paragraph modify.";	
+}
+
+function tempo(){
+	document.getElementById("demo").value;
+
+	if(hora < 20){
+		msg = "Good day!";
+	} else {
+		msg = "Good evening!";
+	}
+
+	x = document.getElementById("demo");
+	x.innerHTML = msg;
+}
